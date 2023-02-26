2cf7a8ed4d40a6d4e2932dc34dc53515158566af
karam-20 <jangirtechs3@gmail.com>
2023-02-26 12:10:45 +0530

    changes in cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_MOBILE],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
